fe7cb6f9cd3fa686e666a8c87685fa536d3c6d49
LuekBucher <lukebucherwork@gmail.com>
2022-03-31 15:45:56 -0400

    Created SQLite db to house matches

--- dota_stats/API_CALL.py
+++ dota_stats/API_CALL.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+import sqlite3 as sql
+from sqlite3 import Error
 #Key 451D543693942BB6C22FC75AC47E3256
 
 
@@ -27,3 +28,52 @@ def init_api_call():
 
 #def backfill_api():
 #TODO Grab init call and compare with current highest match ID from database estimate api calls and add down until backfill is complete
+
+
+def back_fill(last_match_ID,cursor):
+    return 0
+
+    #If no database seen backfill from top down to current "bedrock" match
+    #If database seen backfill until latest match to complete data set
+
+def database_connection(database):
+    try:
+        connection = sql.connect(database)
+        cursor = connection.cursor()
+        return cursor
+    except Error as e:
+        print(e)
+
+
+def get_last_match_ID(cursor):
+    cursor.execute("Select ")
+
+def close_connection(cursor):
+    cursor.connection.commit()
+    cursor.connection.close()
+
+def main():
+    database_URI = "matches.db"
+    db = database_connection(database_URI)
+    create_match_table = "CREATE TABLE IF NOT EXISTS matches(" \
+                         "match_id int PRIMARY KEY," \
+                         "date_start INTEGER, " \
+                         "duration INTEGER," \
+                         " `radiant_hero_1_id` smallint unsigned NOT NULL DEFAULT '0',\
+                        `radiant_hero_2_id` INTEGER NOT NULL DEFAULT '0', \
+                        `radiant_hero_3_id` INTEGER NOT NULL DEFAULT '0',\
+                        `radiant_hero_4_id` INTEGER NOT NULL DEFAULT '0',\
+                        `radiant_hero_5_id` INTEGER NOT NULL DEFAULT '0',\
+                        `dire_hero_1_id` INTEGER NOT NULL DEFAULT '0',\
+                        `dire_hero_2_id` INTEGER NOT NULL DEFAULT '0',\
+                        `dire_hero_3_id` INTEGER NOT NULL DEFAULT '0',\
+                        `dire_hero_4_id` INTEGER NOT NULL DEFAULT '0',\
+                        `dire_hero_5_id` INTEGER NOT NULL DEFAULT '0',\
+                        'winning_team BOOL')"
+    if db is not None:
+        db.execute(create_match_table)
+        close_connection(db)
+
+
+
+main()
